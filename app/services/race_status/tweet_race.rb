5dd92a5a6654e62e7fd48184a644d49e40af91ce
Ifat Ribon <Ifat Ribon>
2017-05-20 17:25:50 -0500

    updated messages

--- app/services/race_status/tweet_race.rb
+++ app/services/race_status/tweet_race.rb
@@ -1,6 +1,6 @@
 module RaceStatus
   class TweetRace
-    attr_reader :user, :new_workout, :workouts, :distance, :speed, :pace, :msg
+    attr_reader :user, :new_workout, :ua_workouts, :distance, :speed, :pace, :msg, :all_workouts
 
     METERS_PER_MILE = 1609.34
 
@@ -19,11 +19,11 @@ module RaceStatus
     private
 
     def fetch_workouts
-      @workouts = UnderArmour::ActivityData.new(user: user).fetch_workouts
+      @ua_workouts = UnderArmour::ActivityData.new(user: user).fetch_workouts
     end
 
     def confirm_new_workout
-      @new_workout = workouts.count > Workout.all.count ? workouts.last : nil
+      @new_workout = ua_workouts.count > Workout.all.count ? ua_workouts.last : nil
     end
 
     def workout_metrics
@@ -31,6 +31,7 @@ module RaceStatus
       @speed = (new_workout['aggregates']['active_time_total'] / 60).round(2)
       @pace = (speed / distance).round(2)
       create_workout
+      @all_workouts = Workout.all
     end
 
     def create_workout
@@ -38,9 +39,10 @@ module RaceStatus
     end
 
     def tweet_metrics
-      num = Workout.all.count
+      num = all_workouts.count
       quote = msg_quotes[num.to_s]
-      @msg = "#{quote} I've run another #{distance} miles in #{speed} minutes, at #{pace} minutes per mile #ChiSpringHalf #RunCHI"
+
+      @msg = "#{quote} at a pace of #{pace} minutes per mile #ChiSpringHalf #RunCHI"
       Twitter::TwitterAPI.new.tweet(msg)
       create_tweet
       Workout.last.update(tweet_id: Tweet.last.id)
@@ -48,12 +50,23 @@ module RaceStatus
 
     def msg_quotes
       {
-        '1' => 'Feeling good!',
-        '2' => 'Almost halfway through!',
-        '3' => 'Going strong!',
-        '4' => 'Last few miles!'
+        '1' => "Feeling good! #{sum_distances} miles and #{sum_times} minutes down,",
+        '2' => "Almost halfway through! #{sum_distances} miles covered in #{sum_times} minutes,",
+        '3' => "Going strong! I've run #{sum_distances} miles in #{sum_times} minutes,",
+        '4' => "Final stretch! #{sum_distances} miles completed in #{sum_times} minutes,"
       }
     end
+
+    def sum_distances
+      all_distances = all_workouts.pluck(:distance)
+      (all_distances.reduce(:+)).round(2)
+    end
+
+    def sum_times
+      all_times = all_workouts.pluck(:speed)
+      (all_times.reduce(:+) + all_times.length * 0.15).round(2)
+    end
+
 
     def create_tweet
       tweet = Tweet.new(date: Date.today, content: msg, user: user, race: Race.first)
